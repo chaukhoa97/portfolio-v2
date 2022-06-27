468f149fbe1cb3c1401d02926814037485813d03
Finn Chau <khoacd@smartosc.com>
2022-06-27 17:14:21 +0700

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: "plugin:react/recommended",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
+};
